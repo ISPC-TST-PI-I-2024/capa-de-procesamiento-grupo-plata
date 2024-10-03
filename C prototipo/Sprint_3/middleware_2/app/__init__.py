10d7522de42a9d80757bf5f798159a3216d10ad1
FerCbr <fercbr@gmail.com>
2024-10-03 18:50:21 -0300

    se sube el codigo de middleware en carpeta prototipo\Sprint_3

---
+++ C prototipo/Sprint_3/middleware_2/app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_jwt_extended import JWTManager
+
+app = Flask(__name__)
+
+app.config['DEBUG'] = True
+
+# Configuración de la clave secreta para JWT
+app.config['JWT_SECRET_KEY'] = 'super-secret'  # Cambia esto por una clave segura
+
+
+
+# Inicialización de JWT
+jwt = JWTManager(app)
+
+from app import routes
